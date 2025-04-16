ec63c760b14a351d5e4f7ffb4a3b19c7430639c1
Alvaro Nicosia <alvaro21k@gmail.com>
2025-04-16 22:33:17 +0200

    created game class

---
+++ openGL2dGame/src/Game/Game.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+enum GameState {
+    GAME_ACTIVE,
+    GAME_MENU,
+    GAME_WIN
+};
+
+class Game {
+public:
+
+	//game state
+	GameState state;
+	bool keys[1024];
+	unsigned int width, height;
+    // constructor/destructor
+    Game(unsigned int width, unsigned int height);
+    ~Game();
+    // initialize game state (load all shaders/textures/levels)
+    void Init();
+    // game loop
+    void ProcessInput(float dt);
+    void Update(float dt);
+    void Render();
+
+    
+};
